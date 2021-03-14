195281fc4a4a32d6eec577c8e542ba3a6cb59a32
lkr27 <lairdkruger@icloud.com>
2021-03-14 23:24:32 +1300

    Installed minimal dependancies

---
+++ src/components/MainHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+export default function MainHeader(props) {
+    return (
+        <div className="main-header-box">
+            <p>React • Webgl Template</p>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainHeader from '../components/MainHeader'
+
+export default function Home() {
+    return (
+        <div>
+            <MainHeader />
+        </div>
+    )
+}
