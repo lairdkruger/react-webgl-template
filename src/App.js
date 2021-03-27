4130a2f313732073fcc5befef727dca50c3385d2
lkr27 <lairdkruger@icloud.com>
2021-03-27 13:05:32 +1300

    added three, added missing sass import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import './Main.scss'
 import Home from './pages/Home'
 
 export default function App() {
